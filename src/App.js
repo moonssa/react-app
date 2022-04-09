e52a70dd39b42d4779b8b89db608809cfebb069e
moonk <mkjoehsb@gmail.com>
2022-04-09 09:57:11 +0900

    useEffect& cleanUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("Created :)  !");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <h1>{showing ? "Welcome back!!" : ""}</h1>
+      <button onClick={onClick}> {showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
