f2598be12a3cae0d7c422979743914c6d946220d
moonk <mkjoehsb@gmail.com>
2022-04-10 10:44:30 +0900

    #7.3 movie fetch with api

--- src/App.js
+++ src/App.js
@@ -1,59 +1,49 @@
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  const [selectedCoin, setSelectedCoin] = useState(0);
-  const [cash, setCash] = useState(0);
-  const onSelect = (event) => {
-    setSelectedCoin(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year",
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const onChange = (event) => {
-    setCash(event.target.value);
-  };
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    // fetch(
+    //   "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year",
+    // )
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     setMovies(json.data.movies);
+    //     setLoading(false);
+    //   });
+    getMovies();
   }, []);
+  console.log(movies);
+
   return (
     <div>
-      <h1> The Coins !! {loading ? "" : `(${coins.length})`} </h1>
       {loading ? (
-        <strong> Loading...</strong>
+        <h1> Loading ... </h1>
       ) : (
         <div>
-          <label forHtml="coin-list">Coin List</label>
-          <select id="coin-list" onChange={onSelect}>
-            <option> Select coin</option>
-            {coins.map((coin) => (
-              <option value={coin.quotes.USD.price}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-              </option>
-            ))}
-          </select>
-
-          <label forHtml="amount"> Investment Amount ($) </label>
-          <input
-            id="amount"
-            onChange={onChange}
-            value={cash}
-            type="number"
-            placeholder="How many dollars?"
-          ></input>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
-
-      <span>
-        You can buy <b />
-        {selectedCoin ? cash / selectedCoin : 0} coins
-      </span>
     </div>
   );
 }
